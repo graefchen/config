68340c8c84d3fc13f142ad0d7e4274eff83f48c9
graef <simonjn45@gmail.com>
2025-11-06 21:02:03 +0100

    small haiku fix

--- nvim/lua/haikus.lua
+++ nvim/lua/haikus.lua
@@ -2,11 +2,8 @@ local list_extend = vim.list_extend
 
 local fmt_haiku = function(haiku)
 	local formatted = { "" }
-	local max = 0
 	for _, line in ipairs(haiku) do
-		local l = string.len(line)
-		max = l > max and l or max
-		formatted = list_extend(formatted, { line })
+		formatted = list_extend(formatted, { string.format("%-40s", line) })
 	end
 	return formatted
 end
@@ -17,19 +14,10 @@ local get_haiku = function(haiku_list)
 	return haiku_list[n]
 end
 
--- Example of how a haiku should look like:
--- [[Cherry blossoms —            ]]
--- [[lights                       ]]
--- [[of years past.               ]]
--- [[                             ]]
--- [[— Matsuo Bashō (Lucien Stryk)]]
--- between the brackets is the translator,
--- if there is no translator, then the
--- brackets aren't present.
+-- NOTE: Does not work on Japanese text.
 -- Mostly from:
 -- -> the book "Lips too chilled" from Pinguin
 -- -> https://www.bopsecrets.org/gateway/passages/basho-frog.htm
--- -> https://en.wikipedia.org/wiki/Matsuo_Bash%C5%8D
 -- -> https://www.poetryfoundation.org/poets/kobayashi-issa
 local haiku_list = {
 	{ lines = { "Cherry blossoms —", "lights", "of years past." }, author = "Matsuo Bashō", translator = "Lucien Stryk" },
@@ -43,15 +31,12 @@ local haiku_list = {
 	{ lines = { "There once was a curious frog", "Who sat by a pond on a log", "And, to see what resulted,", "In the pond catapulted", "With a water-noise heard round the bog." }, author = "Matsuo Bashō", translator = "Alfred H. Marks" },
 	{ lines = { "Within aging pond", "frogs jumping vibrate the calm", "water’s resonance" }, author = "Matsuo Bashō", translator = "Sarah Isbell" },
 	{ lines = { "The old pond;", "a frog jumps in —", "the sound of the water." }, author = "Matsuo Bashō", translator = "Robert Aitken" },
-	{ lines = { "On a branch", "Floating downriver", "a cricket, singing." }, author = "Kobayashi Issa", translator = " Jane Hirshfield" },
-	{ lines = { "甲比丹もつくばはせけり君が春" }, author = "松尾 芭蕉" },
-	{ lines = { "ばしょう植ゑてまづ憎む荻の二葉哉" }, author = "松尾 芭蕉" },
-	{ lines = { "馬をさへながむる雪の朝哉" }, author = "松尾 芭蕉" },
-	{ lines = { "年暮ぬ笠きて草鞋はきながら" }, author = "松尾 芭蕉" },
-	{ lines = { "古池や蛙飛びこむ水の音" }, author = "松尾 芭蕉" },
-	{ lines = { "いざさらば雪見にころぶ所迄" }, author = "松尾 芭蕉" },
-	{ lines = { "荒海や佐渡によこたふ天の川" }, author = "松尾 芭蕉" },
-	{ lines = { "旅に病んで夢は枯野をかけ廻る" }, author = "松尾 芭蕉" },
+	{ lines = { "On a branch", "Floating downriver", "a cricket, singing." }, author = "Kobayashi Issa", translator = "Jane Hirshfield" },
+	{ lines = { "The snow is melting", "and the village is floooded", "    with children." }, author = "Kobayashi Issa", translator = "Robert Hass" },
+	{ lines = { "All the time I pray to Buddha", "I keep on", "   killing mosquitos." }, author = "Kobayashi Issa", translator = "Robert Hass" },
+	{ lines = { "Even with insects—", "some can sing,", "  some can’t." }, author = "Kobayashi Issa", translator = "Robert Hass" },
+	{ lines = { "Goes out,", "comes back—", "      the love life of a cat." }, author = "Kobayashi Issa", translator = "Robert Hass" },
+	{ lines = { "Wake, butterfly—", "it's late, we've miles", "to go together." }, author = "Matsuo Bashō", translator = "Lucien Stryk" },
 }
 
 
@@ -69,7 +54,7 @@ local main = function()
 	if haiku["author"] ~= nil then
 		table.insert(fmt, {
 			type = "text",
-			val = haiku.author,
+			val = string.format("      %-34s", haiku.author),
 			opts = { hl = "Number", position = "center" }
 		})
 	end
@@ -77,7 +62,7 @@ local main = function()
 	if haiku["translator"] ~= nil then
 		table.insert(fmt, {
 			type = "text",
-			val = haiku.translator,
+			val = string.format("      %-34s", haiku.translator),
 			opts = { hl = "String", position = "center" }
 		})
 	end
